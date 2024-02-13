424cfe5b500d75bbb1b7226732267e5f1b571440
Mugika <61477854+mugika19191@users.noreply.github.com>
2024-02-13 17:46:33 +0100

    Creo que asi esta mejor

--- LABO1/main.java
+++ LABO1/main.java
@@ -5,29 +5,29 @@ public class Main {
     public static void main(String[] args) throws Exception {
         ConverterUtils.DataSource source = new ConverterUtils.DataSource(args[0]);
         Instances data = source.getDataSet();
-        // setting class attribute if the data format does not provide this information
-        // For example, the XRFF format saves the class attribute information as well
-        if (data.classIndex() == -1)
-            data.setClassIndex(data.numAttributes() - 1);
-        System.out.println("Lehenengo laborategiko ariketak:");
-        System.out.println("1- Hemendik atera dira datuak: " + args[0]);
-        System.out.println("2- Instantzia kopurua: "+ data.numInstances());
-        System.out.println("3- Atributu kopurua: "+ data.numAttributes());
-        System.out.println("4- Lehenengo atributuak har ditzakeen balio ezberdinak: "+ data.attributeStats(0).distinctCount);
-        System.out.println("5- azken atributuak hartzen dituen balioak eta beraien maiztasuna:");
-        //data.attributeStats(data.numAttributes()-1);
-        String a = "";
-        int b = 0;
-        for (int i=0;i<data.attributeStats(data.numAttributes()-1).nominalCounts.length;i++){
-            System.out.println("Balioa: "+data.attribute(data.numAttributes()-1).value(i) +" Maiztasuna "+data.attributeStats(data.numAttributes()-1).nominalCounts[i]);
-            if (data.attributeStats(data.numAttributes()-1).nominalCounts[i] >= b){
-                b = data.attributeStats(data.numAttributes()-1).nominalCounts[i];
-                a = data.attribute(data.numAttributes()-1).value(i);
-            }
+        data.setClassIndex(data.numAttributes()-1);
+
+        System.out.println("1-Aztertzen ari garen fitxategiko path-a: "+ args[0]);
+        System.out.println("2-Instantzia kopurua: "+ data.numInstances());
+        System.out.println("3-Atributu kopurua: "+data.numAttributes());
+        System.out.println("4-Lehenengo atributuak har ditzakeen balio ezberdinak (balio ezberdin kopurua): "+data.numDistinctValues(0));
+        System.out.println("5-Azken atributuak hartzen dituen balioak eta beraien maiztasuna:");
+        int ind=0;
+        for(int i : data.attributeStats(data.numAttributes()-2).nominalCounts){
+            System.out.println("Izena: "+data.attribute(data.numAttributes()-2).value(ind++)+" || Maiztasuna: "+i);
         }
         System.out.println("");
-        System.out.println("Moda: " + a + " Maiztasuna: " + b);
-        System.out.println("6- Azken aurreko atributuak dituen missing value kopurua: "+ data.attributeStats(data.numAttributes()-2).missingCount );
-
+        System.out.println("6-Zein da klase minoritarioa: ");
+        String minKlas="";
+        int minKlasInt=data.numInstances()+1;
+        for (int i =0;i<data.attributeStats(data.numAttributes()-1).nominalCounts.length;i++){
+            if (minKlasInt>data.attributeStats(data.numAttributes() - 1).nominalCounts[i]) {
+                minKlasInt = data.attributeStats(data.numAttributes() - 1).nominalCounts[i];
+                minKlas = data.attribute(data.numAttributes() - 1).value(i);
+            }
+        }
+        System.out.println("Klase minoritarioa: " +minKlas+" || Maiztasuna: "+minKlasInt);
+        System.out.println("");
+        System.out.println("7-Azken aurreko atributuak dituen missing value kopurua: "+data.attributeStats(data.numAttributes()-3).missingCount);
     }
 }
